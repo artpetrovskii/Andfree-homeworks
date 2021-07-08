a4dbd4dc5fec8cd663398820757ef642c3e6f273
Artemii <artemii.petrovskii@outlook.com>
2021-07-08 18:12:00 +0300

    Lesson2

---
+++ andfree_HW1/app/build/generated/source/buildConfig/androidTest/debug/com/example/andfree_hw1/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.andfree_hw1.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.andfree_hw1.test";
+  public static final String BUILD_TYPE = "debug";
+}
